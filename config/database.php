bb2c341aba515f30e1a915cde39a7fa3881918fe
RagnarLodbrok2017 <35165213+RagnarLodbrok2017@users.noreply.github.com>
2018-11-30 03:19:47 +0200

    make it better

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'solimanacadmic'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_c4220dfebe1953f'),
+            'username' => env('DB_USERNAME', 'b021c0ad54225f'),
+            'password' => env('DB_PASSWORD', '61dbd3cb'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

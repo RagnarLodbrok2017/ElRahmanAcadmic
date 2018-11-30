35774906c8e37903d712b89324af26eeabc6bc51
RagnarLodbrok2017 <35165213+RagnarLodbrok2017@users.noreply.github.com>
2018-11-30 07:02:07 +0200

    Type Seeder

---
+++ database/seeds/TypeTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TypeTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('type')->insert(array(
+            array(
+                'name' => 'حضانة',
+                'description' => 'this is a type',
+            ),
+            array(
+                'name' => 'سنتر',
+                'description' => 'this is a type',
+            ),
+            array(
+                'name' => 'تحفيظ',
+                'description' => 'this is a type',
+            ),
+            array(
+                'name' => 'دار استضافة',
+                'description' => 'this is a type',
+            ),
+            array(
+                'name' => 'أخرى',
+                'description' => 'this is a type',
+            ),
+        ));
+    }
+}

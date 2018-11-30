9bfc6f65a777c46b0008f1c0bb6c7b41df0b6351
RagnarLodbrok2017 <35165213+RagnarLodbrok2017@users.noreply.github.com>
2018-11-30 06:10:03 +0200

    db

--- database/migrations/2018_09_17_182030_create_students_table.php
+++ database/migrations/2018_09_17_182030_create_students_table.php
@@ -14,38 +14,38 @@ class CreateStudentsTable extends Migration
     public function up()
     {
         Schema::create('students', function (Blueprint $table) {
-          $table->increments('id');
-          $table->string('first_name');
-          $table->string('middle_name');
-          $table->string('last_name');
-          $table->boolean('sex');
-          $table->string('region', 50)->nullable();
-          $table->string('phone', 50)->nullable();
-          $table->string('photo')->nullable();
-          $table->date('dob');
-          $table->string('address')->nullable();
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->boolean('sex');
+            $table->string('region', 50)->nullable();
+            $table->string('phone', 50)->nullable();
+            $table->string('photo')->nullable();
+            $table->date('dob');
+            $table->string('address')->nullable();
 
-          $table->unsignedInteger('parents_id')->nullable();
-          $table->foreign('parents_id')->references('id')->on('parents')->onDelete('set null');
+            $table->unsignedInteger('parents_id')->nullable();
+            $table->foreign('parents_id')->references('id')->on('parents')->onDelete('set null');
 
-          $table->unsignedInteger('classroom_id');
-          $table->foreign('classroom_id')->references('id')->on('classroom');
+            $table->unsignedInteger('classroom_id');
+            $table->foreign('classroom_id')->references('id')->on('classroom');
 
-          $table->integer('level_id')->nullable();
-          $table->foreign('level_id')->references('id')->on('level');
+            $table->integer('stage_id')->nullable();
+            $table->foreign('stage_id')->references('id')->on('stage');
 
-          $table->integer('stage_id')->nullable();
-          $table->foreign('stage_id')->references('id')->on('stage');
+            $table->integer('shift_id')->nullable();
+            $table->foreign('shift_id')->references('id')->on('shift');
 
-          $table->integer('shift_id')->nullable();
-          $table->foreign('shift_id')->references('id')->on('shift');
+            $table->unsignedInteger('type_id')->nullable();
+            $table->foreign('type_id')->references('id')->on('type');
+            $table->integer('status_id')->nullable();
+            $table->foreign('status_id')->references('id')->on('status');
+            
+            $table->integer('level_id')->nullable();
+            $table->foreign('level_id')->references('id')->on('level');
 
-          $table->unsignedInteger('type_id')->nullable();
-          $table->foreign('type_id')->references('id')->on('type');
-
-          $table->integer('status_id')->nullable();
-          $table->foreign('status_id')->references('id')->on('status');
-          $table->timestamps();
+            $table->timestamps();
         });
     }
 
